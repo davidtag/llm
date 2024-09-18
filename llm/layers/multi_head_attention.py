5b23ea77172bad41b0f60a7295e441e81c9fbc35
David Tagliamonti <dtag@ambient.ai>
2024-09-17 22:10:46 -0700

    Add MultiHeadAttention impl and its unit tests

---
+++ llm/layers/multi_head_attention.py
@@ -0,0 +1,138 @@
+"""Implements the multi-head attention layer of a Transformer."""
+
+from typing import Optional
+
+import numpy as np
+
+from llm.optimizers import Optimizer
+from llm.utils.math import softmax
+
+
+class MultiHeadAttention(object):
+    """A single multi-head attention sub-layer of a Transformer block.
+
+    TODO(dtag): Support causal masks.
+    """
+    def __init__(
+        self,
+        d_model: int = 512,
+        d_k: int = 64,
+        d_v: int = 64,
+        h: int = 8,
+        enable_grad: bool = True,
+        optimizer: Optional[Optimizer] = None,
+    ) -> None:
+        """Initialize the layer."""
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.h = h
+        self.enable_grad = enable_grad
+        self.optimizer = optimizer
+        self.cache = {}
+
+        self.w_q = np.random.normal(loc=0, scale=np.sqrt(2 / d_model), size=(h, d_model, d_k))
+        self.w_k = np.random.normal(loc=0, scale=np.sqrt(2 / d_model), size=(h, d_model, d_k))
+        self.w_v = np.random.normal(loc=0, scale=np.sqrt(2 / d_model), size=(h, d_model, d_v))
+        self.w_o = np.random.normal(loc=0, scale=np.sqrt(2 / d_model), size=(h * d_v, d_model))
+
+        self.w_q_opt = optimizer.get_parameter_optimizer(self.w_q) if optimizer else None
+        self.w_k_opt = optimizer.get_parameter_optimizer(self.w_k) if optimizer else None
+        self.w_v_opt = optimizer.get_parameter_optimizer(self.w_v) if optimizer else None
+        self.w_o_opt = optimizer.get_parameter_optimizer(self.w_o) if optimizer else None
+
+    @property
+    def n_params(self) -> int:
+        """The number of parameters in the layer."""
+        return (
+            self.w_q.size
+            + self.w_k.size
+            + self.w_v.size
+            + self.w_o.size
+        )
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """Compute the layer output for a given input."""
+        assert x.ndim == 2 and x.shape[-1] == self.d_model
+
+        Q = np.matmul(x, self.w_q)  # shape = (h, N, d_k)
+        K = np.matmul(x, self.w_k)  # shape = (h, N, d_k)
+        V = np.matmul(x, self.w_v)  # shape = (h, N, d_v)
+
+        scale = 1 / np.sqrt(self.d_k)
+        logits = scale * np.matmul(Q, np.transpose(K, axes=[0,2,1]))  # shape = (h, N, N)
+        weights = softmax(logits)  # shape = (h, N, N)
+        head = np.matmul(weights, V)  # shape = (h, N, d_v)
+
+        heads = np.split(head, indices_or_sections=self.h, axis=0)  # List[(1, N, d_v)] of length h
+        heads_squeezed = [head.squeeze() for head in heads]  # List[(N, d_v)] of length h
+        concat = np.hstack(heads_squeezed)  # shape = (N, h * d_v)
+
+        out = np.matmul(concat, self.w_o)  # size = (N, d_model)
+
+        if self.enable_grad:
+            self.cache["x"] = x
+            self.cache["Q"] = Q
+            self.cache["K"] = K
+            self.cache["V"] = V
+            self.cache["weights"] = weights
+            self.cache["concat"] = concat
+
+        return out
+
+    def backward(self, dout: np.ndarray) -> None:
+        """Compute the layer gradients given the upstream gradient."""
+        assert self.enable_grad, "Cannot compute the backward pass with enable_grad=False"
+        x = self.cache["x"]  # shape = (N, d_model)
+        Q = self.cache["Q"]  # shape = (h, N, d_k)
+        K = self.cache["K"]  # shape = (h, N, d_k)
+        V = self.cache["V"]  # shape = (h, N, d_v)
+        weights = self.cache["weights"]  # shape = (h, N, N)
+        concat = self.cache["concat"]  # shape = (N, h * d_v)
+        assert dout.shape == x.shape  # shape = (N, d_model)
+
+        dw_o = np.matmul(np.transpose(concat), dout)  # shape = (h * d_v, d_model)
+        dconcat = np.matmul(dout, np.transpose(self.w_o))  # shape = (N, h * d_v)
+
+        dheads_squeezed = np.hsplit(dconcat, indices_or_sections=self.h)  # List[(N, d_v)] of length h
+        dhead = np.stack(dheads_squeezed, axis=0)  # shape = (h, N, d_v)
+
+        dV = np.matmul(np.transpose(weights, axes=[0,2,1]), dhead)  # shape = (h, N, d_v)
+        dweights = np.matmul(dhead, np.transpose(V, axes=[0,2,1]))  # shape = (h, N, N)
+
+        dlogits = weights * (dweights - np.sum(dweights * weights, axis=2, keepdims=True)) # shape = (h, N, N)
+
+        scale = 1 / np.sqrt(self.d_k)
+        dQ = scale * np.matmul(dlogits, K)  # shape = (h, N, d_k)
+        dK = scale * np.matmul(np.transpose(dlogits, axes=[0,2,1]), Q) # shape = (h, N, d_k)
+
+        dw_q = np.matmul(np.transpose(x), dQ)  # shape = (h, d_model, d_k)
+        dw_k = np.matmul(np.transpose(x), dK)  # shape = (h, d_model, d_k)
+        dw_v = np.matmul(np.transpose(x), dV)  # shape = (h, d_model, d_v)
+
+        dx_from_Q = np.matmul(dQ, np.transpose(self.w_q, axes=[0, 2, 1]))  # shape = (h, N, d_model)
+        dx_from_K = np.matmul(dK, np.transpose(self.w_k, axes=[0, 2, 1]))  # shape = (h, N, d_model)
+        dx_from_V = np.matmul(dV, np.transpose(self.w_v, axes=[0, 2, 1]))  # shape = (h, N, d_model)
+
+        dx = dx_from_Q.sum(axis=0) + dx_from_K.sum(axis=0) + dx_from_V.sum(axis=0)  # shape = (N, d_model)
+
+        self.cache["dx"] = dx
+        self.cache["dw_q"] = dw_q
+        self.cache["dw_k"] = dw_k
+        self.cache["dw_v"] = dw_v
+        self.cache["dw_o"] = dw_o
+
+    def step(self) -> None:
+        """Performs a single optimization step."""
+        assert self.enable_grad, "Cannot take an optimization step with enable_grad=False"
+        assert self.optimizer, "Cannot take an optimization step with optimizer=None"
+
+        assert self.w_q_opt is not None
+        assert self.w_k_opt is not None
+        assert self.w_v_opt is not None
+        assert self.w_o_opt is not None
+
+        self.w_q_opt.step(self.cache["dw_q"])
+        self.w_k_opt.step(self.cache["dw_k"])
+        self.w_v_opt.step(self.cache["dw_v"])
+        self.w_o_opt.step(self.cache["dw_o"])
---
+++ llm/layers/tests/test_multi_head_attention.py
@@ -0,0 +1,168 @@
+"""Unit tests for multi_head_attention.py."""
+
+import unittest
+
+import numpy as np
+
+from llm.layers.multi_head_attention import MultiHeadAttention
+
+
+class TestMultiHeadAttention(unittest.TestCase):
+    """Unit tests for MultiHeadAttention."""
+
+    def setUp(self) -> None:
+        self.data = np.array(
+            [
+                [-0.80672381, -0.08818247, 0.002],
+                [0.63413982, 1.32233656, 0.332],
+                [0.1814214, -0.50674539, -0.0223],
+                [1.16085551, -0.15033837, -0.332],
+            ]
+        )
+
+    def test_n_params(self) -> None:
+        model = MultiHeadAttention(d_model=13, d_k=17, d_v=37, h=7)
+        self.assertEqual(model.n_params, 7 * 13 * (17 + 17 + 37+ 37))
+
+        model = MultiHeadAttention(d_model=14, d_k=64, d_v=37, h=8)
+        self.assertEqual(model.n_params, 8 * 14 * (64 + 64 + 37+ 37))
+
+    def test_forward(self) -> None:
+        model = MultiHeadAttention(d_model=3, d_k=13, d_v=17, h=16)
+
+        out = model.forward(self.data)
+        self.assertEqual(out.shape, (4, 3))
+
+    def test_backward_at_zero(self) -> None:
+        model = MultiHeadAttention(d_model=3, d_k=13, d_v=17, h=16)
+
+        out = model.forward(self.data)
+
+        dout = np.zeros_like(out)
+        model.backward(dout)
+        dx = model.cache["dx"]
+        dw_q = model.cache["dw_q"]
+        dw_k = model.cache["dw_k"]
+        dw_v = model.cache["dw_v"]
+        dw_o = model.cache["dw_o"]
+
+        self.assertEqual(dx.shape, (4, 3))
+        self.assertEqual(dw_q.shape, (16, 3, 13))
+        self.assertEqual(dw_k.shape, (16, 3, 13))
+        self.assertEqual(dw_v.shape, (16, 3, 17))
+        self.assertEqual(dw_o.shape, (16 * 17, 3))
+
+        self.assertTrue(np.all(dx == 0))
+        self.assertTrue(np.all(dw_q == 0))
+        self.assertTrue(np.all(dw_k == 0))
+        self.assertTrue(np.all(dw_v == 0))
+        self.assertTrue(np.all(dw_o == 0))
+
+    def test_backward_at_one_dx(self) -> None:
+        model = MultiHeadAttention(d_model=3)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)  # derivative of loss is 1 for each element
+        model.backward(dout)
+        dx = model.cache["dx"]
+
+        step = 0.01
+        expected_change = step * dx.sum()
+        x = self.data + step
+        out2 = model.forward(x)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=2)  # TODO(dtag): flaky
+
+    def test_backward_at_one_dw_q(self) -> None:
+        model = MultiHeadAttention(d_model=3)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)  # derivative of loss is 1 for each element
+        model.backward(dout)
+        dw_q = model.cache["dw_q"]
+
+        step = 0.01
+        expected_change = step * dw_q.sum()
+        model.w_q += step
+        out2 = model.forward(self.data)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=1)
+
+    def test_backward_at_one_dw_k(self) -> None:
+        model = MultiHeadAttention(d_model=3)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)  # derivative of loss is 1 for each element
+        model.backward(dout)
+        dw_k = model.cache["dw_k"]
+
+        step = 0.01
+        expected_change = step * dw_k.sum()
+        model.w_k += step
+        out2 = model.forward(self.data)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=2)
+
+    def test_backward_at_one_dw_v(self) -> None:
+        model = MultiHeadAttention(d_model=3)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)  # derivative of loss is 1 for each element
+        model.backward(dout)
+        dw_v = model.cache["dw_v"]
+
+        step = 0.01
+        expected_change = step * dw_v.sum()
+        model.w_v += step
+        out2 = model.forward(self.data)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=9)
+
+    def test_backward_at_one_dw_o(self) -> None:
+        model = MultiHeadAttention(d_model=3)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)  # derivative of loss is 1 for each element
+        model.backward(dout)
+        dw_o = model.cache["dw_o"]
+
+        step = 0.01
+        expected_change = step * dw_o.sum()
+        model.w_o += step
+        out2 = model.forward(self.data)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=9)
+
+
+    def test_backward_random_dx(self) -> None:
+        model = MultiHeadAttention(d_model=3)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)  # derivative of loss is 1 for each element
+        model.backward(dout)
+        dx = model.cache["dx"]
+
+        step = 0.01 * np.random.random(size=self.data.shape)
+        expected_change = np.sum(step * dx)
+        x = self.data + step
+        out2 = model.forward(x)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=2)
