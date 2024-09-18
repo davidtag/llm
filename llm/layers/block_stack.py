bb8515d35457c7086197e804c7082b3a50a9aff9
David Tagliamonti <dtag@ambient.ai>
2024-09-17 23:06:39 -0700

    add implementation of block stack

---
+++ llm/layers/block_stack.py
@@ -0,0 +1,84 @@
+"""Implementation of a stack of identical Transformer block layer."""
+
+from typing import Optional
+
+import numpy as np
+
+from llm.layers.block import Block
+from llm.optimizers import Optimizer
+
+
+class BlockStack(object):
+    """A stack of block in a Transformer architecture."""
+
+    def __init__(
+        self,
+        n_blocks: int,
+        d_model: int = 512,
+        d_k: int = 64,
+        d_v: int = 64,
+        h: int = 8,
+        d_ff: int = 2048,
+        enable_grad: bool = True,
+        optimizer: Optional[Optimizer] = None,
+    ) -> None:
+        """Initialize the block stack."""
+        assert n_blocks >= 1
+        self.n_blocks = n_blocks
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.h = h
+        self.d_ff = d_ff
+        self.enable_grad = enable_grad
+        self.optimizer = optimizer
+        self.cache = {}
+
+        self.blocks = [
+            Block(
+                d_model=d_model,
+                d_k=d_k,
+                d_v=d_v,
+                h=h,
+                d_ff=d_ff,
+                enable_grad=enable_grad,
+                optimizer=optimizer,
+            )
+            for _ in range(n_blocks)
+        ]
+
+    @property
+    def n_params(self) -> int:
+        """The number of parameters in the layer."""
+        return sum([block.n_params for block in self.blocks])
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """Compute the layer output for a given input."""
+        assert x.ndim == 2 and x.shape[-1] == self.d_model
+
+        out = x
+
+        for block in self.blocks:
+            out = block.forward(out)
+
+        return out
+
+    def backward(self, dout: np.ndarray) -> None:
+        """Compute the layer gradients given the upstream gradient."""
+        assert self.enable_grad, "Cannot compute the backward pass with enable_grad=False"
+
+        cur_dout = dout
+
+        for block in reversed(self.blocks):
+            block.backward(cur_dout)
+            cur_dout = block.cache["dx"]
+
+        self.cache["dx"] = cur_dout
+
+    def step(self) -> None:
+        """Performs a single optimization step."""
+        assert self.enable_grad, "Cannot take an optimization step with enable_grad=False"
+        assert self.optimizer, "Cannot take an optimization step with optimizer=None"
+
+        for block in self.blocks:
+            block.step()
---
+++ llm/layers/tests/test_block_stack.py
@@ -0,0 +1,96 @@
+"""Unit tests for block_stack.py."""
+
+import unittest
+
+import numpy as np
+
+from llm.layers.block_stack import BlockStack
+
+
+class TestBlockStack(unittest.TestCase):
+    """Unit tests for BlockStack."""
+
+    def setUp(self) -> None:
+        self.data = np.array(
+            [
+                [-0.80672381, -0.08818247, 0.002],
+                [0.63413982, 1.32233656, 0.332],
+                [0.1814214, -0.50674539, -0.0223],
+                [1.16085551, -0.15033837, -0.332],
+            ]
+        )
+
+    def test_n_params(self) -> None:
+        model = BlockStack(n_blocks=2, d_model=13, d_k=17, d_v=37, h=7, d_ff=256)
+        self.assertEqual(
+            model.n_params,
+            2
+            * (
+                # MultiHeadAttention
+                7 * 13 * (17 + 17 + 37 + 37)
+                # LayerNorm
+                + 13 * 2
+                # FeedForward
+                + 13 * 256
+                + 256
+                + 256 * 13
+                + 13
+                # LayerNorm
+                + 13 * 2
+            ),
+        )
+
+    def test_forward(self) -> None:
+        model = BlockStack(n_blocks=5, d_model=3)
+
+        out = model.forward(self.data)
+        self.assertEqual(out.shape, (4, 3))
+
+    def test_backward_at_zero(self) -> None:
+        model = BlockStack(n_blocks=5, d_model=3)
+
+        out = model.forward(self.data)
+
+        dout = np.zeros_like(out)
+        model.backward(dout)
+        dx = model.cache["dx"]
+
+        self.assertEqual(dx.shape, (4, 3))
+
+        self.assertTrue(np.all(dx == 0))
+
+    def test_backward_at_one_dx(self) -> None:
+        model = BlockStack(n_blocks=2, d_model=3)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)  # derivative of loss is 1 for each element
+        model.backward(dout)
+        dx = model.cache["dx"]
+
+        step = 0.01
+        expected_change = step * dx.sum()
+        x = self.data + step
+        out2 = model.forward(x)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=1)
+
+    def test_backward_random_dx(self) -> None:
+        model = BlockStack(n_blocks=2, d_model=3)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)  # derivative of loss is 1 for each element
+        model.backward(dout)
+        dx = model.cache["dx"]
+
+        step = 0.01 * np.random.random(size=self.data.shape)
+        expected_change = np.sum(step * dx)
+        x = self.data + step
+        out2 = model.forward(x)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=1)
