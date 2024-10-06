56a26f498448eed62fe63287082fe9baeb36653c
David Tagliamonti <dtag@ambient.ai>
2024-10-06 10:11:20 -0700

    minor cleanup

--- llm/tokenizers/stdtoken.pyx
+++ llm/tokenizers/stdtoken.pyx
@@ -1,16 +1,19 @@
 """Data types for handling tokenization."""
 
+
 cdef class TokenPair:
     """Stores an immutable pair of tokens.
 
     Requires about 1/2 as much memory and runs in 1/2 the time when constructing and
     storing these objects in a Python dict, as compared to a regular tuple.
+
+    Because it is immutable, it is hashable.
     """
 
     def __cinit__(
         self,
         token_t first,
-        token_t second
+        token_t second,
     ):
         self.first = first
         self.second = second
