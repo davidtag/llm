5f3e5c372f88e73fa7d74788d93396a162891824
David Tagliamonti <dtag@ambient.ai>
2024-10-13 15:17:15 -0700

    Cleanup setup.py and add generated .c files to gitignore

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+"""Build the llm package."""
+
 from setuptools import setup
 from Cython.Build import cythonize
-import numpy
+
 
 setup(
     ext_modules=cythonize(
@@ -10,10 +12,8 @@ setup(
             "llm/tokenizers/cython/stdtoken.pyx",
         ],
         compiler_directives={
-            # "boundscheck": False,
-            # "wraparound": False,
             "language_level": "3str",
         },
     ),
-    include_dirs=[numpy.get_include()],  # Include the numpy headers
+    include_dirs=[],
 )
