fd16a8a6fab5ffd0c7f8ab4d3d95a95b78979bed
David Tagliamonti <dtag@ambient.ai>
2024-10-20 11:38:44 -0700

    move encode logic to Cython

--- llm/tokenizers/bpe.py
+++ llm/tokenizers/bpe.py
@@ -4,8 +4,6 @@ from dataclasses import dataclass
 import unicodedata
 
 from llm.tokenizers.cython import bpe as _bpe_internal
-from llm.tokenizers.cython.frequencies import get_pairwise_tokens
-from llm.tokenizers.cython.merge import merge
 from llm.tokenizers.cython.stdtoken import TokenPair
 
 
@@ -66,24 +64,9 @@ def convert_vocabulary_to_piece_cache(vocab: Vocabulary) -> PieceCache:
     return piece_cache
 
 
-def encode_tokens(tokens: list[int], merge_dict: MergeDict) -> list[int]:
-    """Encode a sequence of tokens according to a merge priority."""
-    while len(tokens) > 1:
-        all_pairs = get_pairwise_tokens(tokens)
-        merge_pair = min(all_pairs, key=lambda p: merge_dict.get(p, float("inf")))  # token value = priority
-        replacement = merge_dict.get(merge_pair, None)
-        if replacement is None:
-            break
-        tokens = merge(tokens, merge_pair, replacement)
-    return tokens
-
-
 def encode_piece(piece: str, merge_dict: MergeDict) -> list[int]:
     """Encode text according to a merge priority."""
-    piece_bytes = piece.encode("utf-8")
-    piece_base_tokens = list(piece_bytes)
-    piece_tokens = encode_tokens(piece_base_tokens, merge_dict=merge_dict)
-    return piece_tokens
+    return _bpe_internal.encode_piece(piece, merge_dict)
 
 
 def decode_bytes(tokens: list[int], vocab: Vocabulary) -> bytes:
--- llm/tokenizers/cython/bpe.pyx
+++ llm/tokenizers/cython/bpe.pyx
@@ -1,6 +1,30 @@
 """Utilities for implementing the Byte-Pair Encoding (BPE) algorithm."""
 
+from llm.tokenizers.cython.frequencies import get_pairwise_tokens  # TODO(dtag): Add pxd so I can cimport
+from llm.tokenizers.cython.merge import merge  # TODO(dtag): Add pxd so I can cimport
+from llm.tokenizers.cython.stdtoken cimport TokenPair
+
 import cython
+
+
+def encode_tokens(tokens: list[int], merge_dict: dict[TokenPair, int]) -> list[int]:
+    """Encode a sequence of tokens according to a merge priority."""
+    while len(tokens) > 1:
+        all_pairs = get_pairwise_tokens(tokens)
+        merge_pair = min(all_pairs, key=lambda p: merge_dict.get(p, float("inf")))  # token value = priority
+        replacement = merge_dict.get(merge_pair, None)
+        if replacement is None:
+            break
+        tokens = merge(tokens, merge_pair, replacement)
+    return tokens
+
+
+def encode_piece(piece: str, merge_dict: dict[TokenPair, int]) -> list[int]:
+    """Encode text according to a merge priority."""
+    piece_bytes = piece.encode("utf-8")
+    piece_base_tokens = list(piece_bytes)
+    piece_tokens = encode_tokens(piece_base_tokens, merge_dict=merge_dict)
+    return piece_tokens
 
 
 @cython.boundscheck(False)
