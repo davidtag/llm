ee6795c1ced36c5214a967e5902d8338dca9bb62
David Tagliamonti <dtag@ambient.ai>
2024-11-10 15:48:42 -0800

    Add image embedding layer

---
+++ llm/layers/image_embedding.py
@@ -0,0 +1,169 @@
+"""A learned image embedding layer."""
+
+from typing import Optional
+
+import numpy as np
+
+from llm.constants import DType, DEFAULT_DTYPE, BaseParameter, Parameters
+from llm.layers.base import Layer
+from llm.optimizers import Optimizer
+
+
+class ImageEmbedding(Layer):
+    """A learned image embedding layer."""
+
+    def __init__(
+        self,
+        patch_size: int,
+        canonical_width: int = 224,
+        canonical_height: int = 224,
+        n_channel: int = 3,
+        d_model: int = 512,
+        dtype: DType = DEFAULT_DTYPE,
+        enable_grad: bool = True,
+        optimizer: Optional[Optimizer] = None,
+    ) -> None:
+        """Initialize the layer."""
+        super().__init__(dtype=dtype, enable_grad=enable_grad, optimizer=optimizer)
+        self.patch_size = patch_size  # P
+        self.canonical_width = canonical_width  # W
+        self.canonical_height = canonical_height  # H
+        self.n_channel = n_channel  # C
+        self.d_model = d_model
+
+        if canonical_width % patch_size != 0:
+            raise ValueError("Width must be a multiple of patch size")
+
+        if canonical_height % patch_size != 0:
+            raise ValueError("Height must be a multiple of patch size")
+
+        self.d_patch = (patch_size**2) * n_channel  # (P^2)*C
+        n_patches = (canonical_height * canonical_width) // (patch_size**2)  # N
+        self.context_size = n_patches + 1  # N + 1: [class token, *patch_tokens]
+
+        self.class_token = np.random.standard_normal(size=(1, d_model)).astype(dtype)
+        spread = np.sqrt(1 / self.d_patch)
+        self.patch_proj = np.random.uniform(
+            low=-spread,
+            high=spread,
+            size=(self.d_patch, self.d_model),
+        ).astype(dtype)
+        self.position_embedding_matrix = np.random.standard_normal(
+            size=(self.context_size, self.d_model)
+        ).astype(dtype)
+
+        self.class_token_opt = optimizer.get_parameter_optimizer(self.class_token) if optimizer else None
+        self.patch_proj_opt = optimizer.get_parameter_optimizer(self.patch_proj) if optimizer else None
+        self.position_embedding_matrix_opt = (
+            optimizer.get_parameter_optimizer(self.position_embedding_matrix) if optimizer else None
+        )
+
+        # Pre-allocate the gradient to save compute on each backward pass and avoid large memory allocations
+        self.cache["dposition_embedding_matrix"] = np.zeros_like(self.position_embedding_matrix)
+
+    @property
+    def n_params(self) -> int:
+        """The number of parameters in the layer."""
+        return self.class_token.size + self.patch_proj.size + self.position_embedding_matrix.size
+
+    def get_parameters(self) -> Parameters:
+        """Return the parameter map for the layer."""
+        params = {
+            "class_token": self.class_token,
+            "patch_proj": self.patch_proj,
+            "position_embedding_matrix": self.position_embedding_matrix,
+        }
+        return params
+
+    def load_parameters(self, params: Parameters) -> None:
+        """Set the parameters."""
+        if (
+            "class_token" not in params
+            or "patch_proj" not in params
+            or "position_embedding_matrix" not in params
+        ):
+            raise ValueError("Missing parameters")
+        if (
+            not isinstance(params["class_token"], BaseParameter)
+            or not isinstance(params["patch_proj"], BaseParameter)
+            or not isinstance(params["position_embedding_matrix"], BaseParameter)
+        ):
+            raise ValueError("Invalid shape for parameters map")
+        self.class_token[:] = params["class_token"]
+        self.patch_proj[:] = params["patch_proj"]
+        self.position_embedding_matrix[:] = params["position_embedding_matrix"]
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """Compute the layer output for a given input."""
+        assert x.ndim == 4  # (B, H, W, C)
+        B, H, W, C = x.shape
+        assert H % self.patch_size == 0
+        assert W % self.patch_size == 0
+        assert C == self.n_channel
+
+        N = H * W // (self.patch_size**2)
+        assert N == self.context_size - 1  # TODO(dtag): Support variable size inputs using 2D proj
+
+        # Patch Embeddings: Flatten + Project
+        patches = x.reshape(B, N, self.d_patch)  # (B, N, d_patch)
+        patch_embeddings = np.matmul(patches, self.patch_proj)  # (B, N, d_model)
+
+        # Class Token Embedding: Broadcast
+        class_token_embeddings = np.broadcast_to(self.class_token, (B, 1, self.d_model))  # (B, 1, d_model)
+
+        # Token Embeddings: Prepend class token to patch tokens
+        token_embeddings = np.concatenate(  # (B, N+1, d_model)
+            (class_token_embeddings, patch_embeddings),
+            axis=1,
+        )
+
+        # Position Embeddings: Lookup
+        position_embeddings = self.position_embedding_matrix[: N + 1]  # (N + 1, d_model)
+
+        # Final Embeddings: Token + Position
+        out = token_embeddings + position_embeddings  # (B, N+1, d_model)
+
+        if self.enable_grad:
+            self.cache["x"] = x
+
+        return out
+
+    def backward(self, dout: np.ndarray) -> None:
+        """Compute the layer gradients given the upstream gradient."""
+        assert self.enable_grad, "Cannot compute the backward pass with enable_grad=False"
+        x = self.cache["x"]
+        B, H, W, C = x.shape
+        N = H * W // (self.patch_size**2)
+        assert dout.shape == (B, N + 1, self.d_model)
+
+        # Zero-out static gradient matrices instead of re-allocating on each pass
+        dposition_embedding_matrix = self.cache["dposition_embedding_matrix"]
+        dposition_embedding_matrix[:] = 0
+        dposition_embedding_matrix[: N + 1] = dout.sum(axis=0)
+        self.cache["dposition_embedding_matrix"] = dposition_embedding_matrix
+
+        dtoken_embeddings = dout
+        dclass_token_embeddings = dtoken_embeddings[:, 0, :]  # (B, 1, d_model)
+        dpatch_embeddings = dtoken_embeddings[:, 1:, :]  # (B, N, d_model)
+
+        dclass_token = dclass_token_embeddings.sum(axis=0, keepdims=True)
+        self.cache["dclass_token"] = dclass_token
+
+        patches = x.reshape(B, N, self.d_patch)  # (B, N, d_patch)
+        patches_t = np.transpose(patches, axes=(0, -1, -2))  # (B, d_patch, N)
+        dpatch_proj = np.matmul(patches_t, dpatch_embeddings)  # (B, d_patch, d_model)
+        dpatch_proj = np.sum(dpatch_proj, axis=0)
+        self.cache["dpatch_proj"] = dpatch_proj
+
+    def step(self) -> None:
+        """Perform a single optimization step."""
+        assert self.enable_grad, "Cannot take an optimization step with enable_grad=False"
+        assert self.optimizer, "Cannot take an optimization step with optimizer=None"
+
+        assert self.class_token_opt is not None
+        assert self.patch_proj_opt is not None
+        assert self.position_embedding_matrix_opt is not None
+
+        self.class_token_opt.step(self.cache["dclass_token"])
+        self.patch_proj_opt.step(self.cache["dpatch_proj"])
+        self.position_embedding_matrix_opt.step(self.cache["dposition_embedding_matrix"])
---
+++ llm/layers/tests/test_image_embedding.py
@@ -0,0 +1,191 @@
+"""Unit tests for image_embedding.py."""
+
+import operator
+import unittest
+
+import numpy as np
+
+from llm.constants import DEFAULT_DTYPE
+from llm.layers.image_embedding import ImageEmbedding
+
+
+class TestImageEmbedding(unittest.TestCase):
+    """Unit tests for ImageEmbedding."""
+
+    def setUp(self) -> None:
+        self.data = np.random.standard_normal(size=(3, 16, 16, 3))  # batch of 3 normalized images
+
+    def test_n_params(self) -> None:
+        """Test the layer reports the correct number of parameters."""
+        model = ImageEmbedding(patch_size=8, canonical_width=16, canonical_height=16, n_channel=3, d_model=64)
+        self.assertEqual(
+            model.n_params,
+            (
+                1 * 64  # class_token
+                + 192 * 64  # patch_proj: 192 = 8 * 8 * 3
+                + (4 + 1) * 64  # position_embedding_matrix
+            ),
+        )
+
+    def test_get_parameters(self) -> None:
+        """Test the get_parameters() method."""
+        model = ImageEmbedding(patch_size=8, canonical_width=16, canonical_height=16, n_channel=3, d_model=64)
+        params = model.get_parameters()
+        self.assertSetEqual(set(params.keys()), {"class_token", "patch_proj", "position_embedding_matrix"})
+        token = params["class_token"]
+        proj = params["patch_proj"]
+        pos = params["position_embedding_matrix"]
+        assert isinstance(token, np.ndarray)
+        assert isinstance(proj, np.ndarray)
+        assert isinstance(pos, np.ndarray)
+        self.assertEqual(token.shape, (1, 64))
+        self.assertEqual(proj.shape, (192, 64))
+        self.assertEqual(pos.shape, (5, 64))
+        self.assertEqual(token.dtype, DEFAULT_DTYPE)
+        self.assertEqual(proj.dtype, DEFAULT_DTYPE)
+        self.assertEqual(pos.dtype, DEFAULT_DTYPE)
+
+    def test_load_parameters(self) -> None:
+        """Test the load_paramters() method."""
+        model = ImageEmbedding(patch_size=8, canonical_width=16, canonical_height=16, n_channel=3, d_model=64)
+        out1 = model.forward(self.data)
+
+        token_new = np.zeros((1, 64), dtype=DEFAULT_DTYPE)
+        proj_new = np.zeros((192, 64), dtype=DEFAULT_DTYPE)
+        pos_new = np.zeros((5, 64), dtype=DEFAULT_DTYPE)
+        params = {
+            "class_token": token_new,
+            "patch_proj": proj_new,
+            "position_embedding_matrix": pos_new,
+        }
+
+        model.load_parameters(params)
+        out2 = model.forward(self.data)
+        np.testing.assert_array_compare(operator.__ne__, out1, out2)
+
+    def test_get_parameters_and_load_parameters_roundtrip(self) -> None:
+        """Test that the return value of get_parameters() can be loaded."""
+        model = ImageEmbedding(patch_size=8, canonical_width=16, canonical_height=16, n_channel=3, d_model=64)
+        out1 = model.forward(self.data)
+
+        params = model.get_parameters()
+
+        model.load_parameters(params)
+        out2 = model.forward(self.data)
+        np.testing.assert_array_equal(out1, out2)
+
+    def test_forward(self) -> None:
+        """Test the forward pass."""
+        model = ImageEmbedding(patch_size=8, canonical_width=16, canonical_height=16, n_channel=3, d_model=64)
+
+        out = model.forward(self.data)
+        self.assertEqual(out.shape, (3, 4 + 1, 64))  # W/H of 16 + patch_size=8 -> 4 patches; +1 class token
+
+    def test_backward_at_zero(self) -> None:
+        """Test the backward pass with upstream gradient being 0."""
+        model = ImageEmbedding(patch_size=8, canonical_width=16, canonical_height=16, n_channel=3, d_model=64)
+
+        out = model.forward(self.data)
+
+        dout = np.zeros_like(out)
+        model.backward(dout)
+
+        dclass_token = model.cache["dclass_token"]
+        dpatch_proj = model.cache["dpatch_proj"]
+        dposition_embedding_matrix = model.cache["dposition_embedding_matrix"]
+
+        self.assertEqual(dclass_token.shape, (1, 64))
+        self.assertEqual(dpatch_proj.shape, (192, 64))
+        self.assertEqual(dposition_embedding_matrix.shape, (5, 64))
+
+        self.assertTrue(np.all(dclass_token == 0))
+        self.assertTrue(np.all(dpatch_proj == 0))
+        self.assertTrue(np.all(dposition_embedding_matrix == 0))
+
+    def test_backward_at_one(self) -> None:
+        """Test the backward pass with upstream gradient being 1."""
+        model = ImageEmbedding(patch_size=8, canonical_width=16, canonical_height=16, n_channel=3, d_model=64)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)  # derivative of loss is 1 for each element
+        model.backward(dout)
+
+        dclass_token = model.cache["dclass_token"]
+        dpatch_proj = model.cache["dpatch_proj"]
+        dposition_embedding_matrix = model.cache["dposition_embedding_matrix"]
+
+        # Class Token
+        # -> All positions have gradient of 3 (batch_size) because they're directly added to the output
+        self.assertTrue(np.all(dclass_token == 3))
+
+        # Position Embedding Matrix
+        # -> All positions have gradient of 3 (batch_size) because each position is represented in the input
+        self.assertTrue(np.all(dposition_embedding_matrix == 3))
+
+        # Patch Projection Matrix
+        step = 0.01
+        expected_change = step * dpatch_proj.sum()
+        model.patch_proj += step
+        out2 = model.forward(self.data)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=9)
+
+    def test_backward_dclass_token_random(self) -> None:
+        """Test the backward pass for the class_token gradient with random step."""
+        model = ImageEmbedding(patch_size=8, canonical_width=16, canonical_height=16, n_channel=3, d_model=64)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)
+        model.backward(dout)
+        dclass_token = model.cache["dclass_token"]
+
+        step = np.random.normal(loc=0, scale=0.01, size=model.class_token.shape)
+        expected_change = np.sum(step * dclass_token)
+        model.class_token += step
+        out2 = model.forward(self.data)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=9)
+
+    def test_backward_dpatch_proj_random(self) -> None:
+        """Test the backward pass for the patch_proj gradient with random step."""
+        model = ImageEmbedding(patch_size=8, canonical_width=16, canonical_height=16, n_channel=3, d_model=64)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)
+        model.backward(dout)
+        dpatch_proj = model.cache["dpatch_proj"]
+
+        step = np.random.normal(loc=0, scale=0.01, size=model.patch_proj.shape)
+        expected_change = np.sum(step * dpatch_proj)
+        model.patch_proj += step
+        out2 = model.forward(self.data)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=9)
+
+    def test_backward_dposition_random(self) -> None:
+        """Test the backward pass for position gradients with random step."""
+        model = ImageEmbedding(patch_size=8, canonical_width=16, canonical_height=16, n_channel=3, d_model=64)
+
+        out = model.forward(self.data)
+        loss = out.sum()
+
+        dout = np.ones_like(out)
+        model.backward(dout)
+        dposition_embedding_matrix = model.cache["dposition_embedding_matrix"]
+
+        step = np.random.normal(loc=0, scale=0.01, size=model.position_embedding_matrix.shape)
+        expected_change = np.sum(step * dposition_embedding_matrix)
+        model.position_embedding_matrix += step
+        out2 = model.forward(self.data)
+        loss2 = out2.sum()
+        actual_change = loss2 - loss
+        self.assertAlmostEqual(actual_change, expected_change, places=9)
