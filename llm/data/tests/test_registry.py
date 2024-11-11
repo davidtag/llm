142e40ea90c7ea4b20b925ea0945060062297c14
David Tagliamonti <dtag@ambient.ai>
2024-11-10 22:44:48 -0800

    Add more unit tests for registry.py

--- llm/data/tests/test_registry.py
+++ llm/data/tests/test_registry.py
@@ -8,6 +8,7 @@ from llm.data.registry import (
     TokenizerRegistry,
     TokenRegistry,
     ModelRegistry,
+    ImageDataRegistry,
 )
 
 
@@ -54,3 +55,20 @@ class TestModelRegistry(unittest.TestCase):
         registry = ModelRegistry()
 
         self.assertTrue(registry.checkpoint_dir.as_posix().endswith("assets/model_checkpoints"))
+
+
+class TestImageDataRegistry(unittest.TestCase):
+    """Unit tests for ImageDataRegistry."""
+
+    def test_all(self) -> None:
+        registry = ImageDataRegistry()
+
+        self.assertTrue(registry.image_dir.as_posix().endswith("assets/images"))
+        self.assertTrue(registry.tar_file.as_posix().endswith("assets/images/cifar-10-python.tar.gz"))
+        self.assertTrue(registry.splits_dir.as_posix().endswith("assets/images/cifar-10-batches-py"))
+        self.assertTrue(
+            registry.train_files[0].as_posix().endswith("assets/images/cifar-10-batches-py/data_batch_1")
+        )
+        self.assertTrue(
+            registry.test_file.as_posix().endswith("assets/images/cifar-10-batches-py/test_batch")
+        )
